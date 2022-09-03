04e7885c27d8b5e9ee5ed8a2f8031bd9ca193ef5
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2022-09-04 00:43:43 +0200

    Add eslint & prettier

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
 import Fastify from 'fastify'
 
 const fastify = Fastify({
-	logger: true,
+  logger: true
 })
 
-fastify.get('/', (req, res) => {
-	res.send({ message: 'Hello from the api!' })
+fastify.get('/', () => {
+  return { message: 'Hello from the api!' }
 })
 
-fastify.listen({ port: 3000 }, (err, address) => {
-	if (err) {
-		fastify.log.error(err)
-		process.exit(1)
-	}
-	console.log(`Server is now listening on ${address}`)
-})
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3000 })
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+void start()
