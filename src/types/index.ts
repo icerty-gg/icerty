6a41ec1b76582249bde258cedb4b355ad7947254
Stanisław Jakubowski <sjakubowski3554@gmail.com>
2022-10-19 22:38:11 +0200

    Delete types

--- src/types/index.ts
+++
@@ -1,6 +0,0 @@
-import type { TSchema } from '@sinclair/typebox'
-import type { FastifySchema } from 'fastify'
-
-export type TypeBoxFastifySchema = Partial<Record<keyof Omit<FastifySchema, 'response'>, TSchema>> & {
-  readonly response?: { readonly [statusCode: number]: TSchema }
-}
