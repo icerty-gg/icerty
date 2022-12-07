b831bf585eac78f5b739bba6c102883b282f4fd6
Mateusz <michalikkontakt@gmail.com>
2022-12-07 15:05:30 +0100

    add: added inter font

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
+import { Inter } from '@next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-inter'
+})
 
 const RootLayout = ({ children }: { readonly children: React.ReactNode }) => {
   return (
-    <html>
+    <html className={`${inter.variable} font-sans`} lang='en'>
       <body>{children}</body>
     </html>
   )
--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,16 +1,8 @@
-import { Inter } from '@next/font/google'
-
 import type { AppProps } from 'next/app'
-
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-  variable: '--font-inter'
-})
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <main className={`${inter.variable} font-sans`}>
+    <main>
       <Component {...pageProps} />
     </main>
   )
