4cd4f69012fd56c39dc07fd88b0714bb8264aaab
Mateusz <michalikkontakt@gmail.com>
2022-12-09 01:00:06 +0100

    add: added new page

---
+++ apps/client/app/add-announcement/page.tsx
@@ -0,0 +1,9 @@
+const AddAnnouncement = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-full'>
+      <h1 className='text-3xl text-left'>essa</h1>
+    </div>
+  )
+}
+
+export default AddAnnouncement
