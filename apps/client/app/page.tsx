c4c83a691521a11b2441f1e1e5bb98385f639a69
Mateusz <michalikkontakt@gmail.com>
2022-12-13 00:59:52 +0100

    update: switched files of fetching

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,11 +1,30 @@
-import { CategoryList } from '../components/categories/CategoryList'
-import { Wrapper } from '../components/ui/Wrapper'
+import { getCategoriesSchema, validateSchema } from 'common'
 
-const Home = () => {
+import { CategoryItem } from '../components/categories/CategoryItem'
+import { Wrapper } from '../components/ui/Wrapper'
+import { api } from '../constants/api'
+
+const getCategories = async () => {
+  const schema = getCategoriesSchema['response'][200]
+
+  const { data } = await api.get('/categories')
+
+  const { categories } = validateSchema(schema, data)
+
+  return categories
+}
+
+const Home = async () => {
+  const categories = await getCategories()
+
   return (
     <Wrapper>
       <h1>Kategorie główne</h1>
-      <CategoryList />
+      <ul>
+        {categories.map(c => (
+          <CategoryItem key={c.id} name={c.name} image={c.img} />
+        ))}
+      </ul>
     </Wrapper>
   )
 }
