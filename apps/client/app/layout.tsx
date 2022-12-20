36701f1ee7afeab2b731999de64022ba5fd76798
Mateusz <michalikkontakt@gmail.com>
2022-12-20 16:05:37 +0100

    fix: fixed semantic html

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from '@next/font/google'
 
 import { Navbar } from '../components/navbar/Navbar'
 import { BackToTopButton } from '../components/ui/BackToTopButton'
+import { BluredCircle } from '../components/ui/BluredCircle'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -15,7 +16,10 @@ const RootLayout = ({ children }: { readonly children: React.ReactNode }) => {
     <html className={`${inter.variable} font-sans`} lang='en'>
       <body className='bg-gray-900 relative'>
         <Navbar />
-        <div className='py-24'>{children}</div>
+        <div className='py-24'>
+          {children}
+          <BluredCircle />
+        </div>
         <BackToTopButton />
       </body>
     </html>
