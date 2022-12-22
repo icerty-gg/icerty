5d60fb350c7f07a599aafe2037eaf66fe2345a88
Mateusz <michalikkontakt@gmail.com>
2022-12-22 04:53:51 +0100

    update: updated font size

--- apps/client/components/filter/CheckboxInput.tsx
+++ apps/client/components/filter/CheckboxInput.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export const CheckboxInput = ({ children, error, validate }: Props) => {
   return (
-    <>
-      <label className='text-white flex items-center gap-4 cursor-pointer'>
+    <div className='flex flex-col gap-4 w-full'>
+      <label className='text-white flex items-center gap-4 cursor-pointer text-sm'>
         <input
           type='checkbox'
           className='p-2 w-[1.3rem] h-[1.3rem] cursor-pointer text-sky-500 rounded-full border border-slate-800 checked:bg-sky-500 appearance-none'
@@ -16,6 +16,6 @@ export const CheckboxInput = ({ children, error, validate }: Props) => {
         {children}
       </label>
       {error}
-    </>
+    </div>
   )
 }
