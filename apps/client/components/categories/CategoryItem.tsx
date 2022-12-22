ee1261bb52ac5f24cfccfcabf4a41361def1c1cc
Mateusz <michalikkontakt@gmail.com>
2022-12-22 04:05:02 +0100

    update: changed language

--- apps/client/components/categories/CategoryItem.tsx
+++ apps/client/components/categories/CategoryItem.tsx
@@ -13,10 +13,16 @@ export const CategoryItem = ({ href, image, isSmall, name }: Props) => {
     <li className='border bg-gray-800/20 border-slate-800 hover:bg-sky-400/20 rounded-xl'>
       <Link
         className={`flex ${isSmall ? 'flex-row p-2' : 'flex-col p-4'} items-center gap-6`}
-        href={`announcements/${href.toLowerCase()}`}
+        href={`offers/${href.toLowerCase()}`}
       >
         <div className='flex items-center justify-center bg-sky-400/10 rounded-full'>
-          <Image className='pointer-events-none' width={isSmall ? 60 : 80} height={isSmall ? 60 : 80} src={image} alt={name} />
+          <Image
+            className='pointer-events-none rounded-md'
+            width={isSmall ? 80 : 100}
+            height={isSmall ? 80 : 100}
+            src={image}
+            alt={name}
+          />
         </div>
 
         <h3 className='text-white text-center'>{name}</h3>
