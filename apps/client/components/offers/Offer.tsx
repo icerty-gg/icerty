6319cd45618e0e03921871bbe4e25983c59fabf1
Mateusz <michalikkontakt@gmail.com>
2022-12-23 05:31:43 +0100

    update: updated iso date to locale

--- apps/client/components/offers/Offer.tsx
+++ apps/client/components/offers/Offer.tsx
@@ -10,16 +10,18 @@ interface Props {
 }
 
 export const Offer = ({ createdAt, id, image, name, price }: Props) => {
-  console.log('kurwo --->', image)
+  const date = new Date(createdAt)
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' } as const
+
   return (
     <Link href={`/offers/${id}`}>
       <li className='flex items-center gap-4 border bg-gray-800/20 border-slate-800 hover:bg-sky-400/20 p-4 rounded-xl'>
         <Image width={130} height={130} src={image} alt={name} className='rounded-md max-md:w-[70px]' />
 
         <div className='text-white flex flex-col gap-2'>
-          <p className='text-xs'>Dodane {createdAt}</p>
+          <p className='text-xs'>Added at {date.toLocaleDateString('us-US', options)}</p>
           <h3 className='line-clamp-none md:line-clamp-2'>{name}</h3>
-          <p className='text-sky-500 text-xl font-bold'>{price} zł</p>
+          <p className='text-sky-500 text-xl font-bold'>{price} USD</p>
         </div>
       </li>
     </Link>
