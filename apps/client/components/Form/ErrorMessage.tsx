47cab0066869ae637e62db4de857a54db5307aaf
Mateusz <michalikkontakt@gmail.com>
2022-12-23 05:13:14 +0100

    fix: fixed a11y

---
+++ apps/client/components/Form/ErrorMessage.tsx
@@ -0,0 +1,9 @@
+import { BiErrorCircle } from 'react-icons/bi'
+
+export const ErrorMessage = ({ children }: { readonly children: React.ReactNode }) => {
+  return (
+    <p className='text-red-700 flex items-center gap-2 text-sm'>
+      <BiErrorCircle className='text-lg' /> {children}
+    </p>
+  )
+}
