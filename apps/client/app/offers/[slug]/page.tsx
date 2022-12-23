0c01715933caffe885b6870a7a6966dc67d85d05
Mateusz <michalikkontakt@gmail.com>
2022-12-24 00:36:54 +0100

    feat: added fetching at dynamic route

--- apps/client/app/offers/[slug]/page.tsx
+++ apps/client/app/offers/[slug]/page.tsx
@@ -1,3 +1,26 @@
-const OfferDetails = () => <p>ok</p>
+import { api } from '../../../utils/fetcher'
+
+export const generateStaticParams = async () => {
+  const { data: offers } = await api.get('/offers/')
+
+  return offers.map(o => ({
+    slug: o.id
+  }))
+}
+
+interface Props {
+  readonly params: { readonly slug: string }
+}
+
+const OfferDetails = async ({ params }: Props) => {
+  const { slug } = params
+  const offer = await api.get('/offers/:id', { params: { id: slug } })
+
+  return (
+    <div>
+      <h1>{offer.name}</h1>
+    </div>
+  )
+}
 
 export default OfferDetails
