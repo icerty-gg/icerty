e40d200d268dac17d0f0c7e170ed44ecf9abaa83
Mateusz <michalikkontakt@gmail.com>
2022-12-26 03:33:38 +0100

    fix: fixed jsx structure

--- apps/client/app/login/page.tsx
+++ apps/client/app/login/page.tsx
@@ -1,7 +1,13 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import React, { InputHTMLAttributes } from 'react'
+import {
+  FieldValues,
+  UseFormRegister,
+  // useForm, // don't need this import
+  useForm
+} from 'react-hook-form'
 import { BiLockAlt, BiMailSend } from 'react-icons/bi'
 import { z } from 'zod'
 
@@ -22,7 +28,7 @@ const LoginSchema = z.object({
 
 type FormSchemaType = z.infer<typeof LoginSchema>
 
-const LoginForm = () => {
+const Login = () => {
   const {
     formState: { errors },
     handleSubmit,
@@ -36,38 +42,33 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-2 gap-6'>
-      <Input
-        validate={{ ...register('email') }}
-        className='col-span-2'
-        icon={<BiMailSend className='text-lg' />}
-        type='email'
-        label='Email'
-        error={errors.email?.message && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
-      />
-      <Input
-        validate={{ ...register('password') }}
-        className='col-span-2'
-        icon={<BiLockAlt className='text-lg' />}
-        type='password'
-        label='Password'
-        error={errors.password?.message && <ErrorMessage>{errors.password?.message}</ErrorMessage>}
-      />
-
-      <PrimaryButton isFormTypeButton={true} className='text-sm col-span-2' href='/'>
-        Login
-      </PrimaryButton>
-    </form>
-  )
-}
-
-const Login = () => {
-  return (
     <Layout>
       <div className='grid grid-cols-1 gap-4 w-full max-w-[35rem] m-auto'>
         <Container>
           <Heading title='Login to your account' className='pb-6' />
-          <LoginForm />
+          <form onSubmit={handleSubmit(onSubmit)} className='grid grid-cols-2 gap-6'>
+            <Input
+              register={{ ...register('email') }}
+              className='col-span-2'
+              icon={<BiMailSend className='text-lg' />}
+              type='email'
+              label='Email'
+              error={errors.email?.message && <ErrorMessage>{errors.email?.message}</ErrorMessage>}
+            />
+            <Input
+              register={{ ...register('password') }}
+              className='col-span-2'
+              icon={<BiLockAlt className='text-lg' />}
+              type='password'
+              label='Password'
+              isPasswordType={true}
+              error={errors.password?.message && <ErrorMessage>{errors.password?.message}</ErrorMessage>}
+            />
+
+            <PrimaryButton isFormTypeButton={true} className='text-sm col-span-2' href='/'>
+              Login
+            </PrimaryButton>
+          </form>
         </Container>
         <div className='flex flex-col gap-4 items-center p-4 rounded-xl border bg-gray-800/20 border-slate-800'>
           <p className='text-white'>You dont have an account?</p>
