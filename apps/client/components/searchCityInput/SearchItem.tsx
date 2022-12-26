b919bfdda4c07176f8f59ce9ae6b51f4e2aab186
Mateusz <michalikkontakt@gmail.com>
2022-12-26 03:35:48 +0100

    fix: fixed jsx structure

--- apps/client/components/searchCityInput/SearchItem.tsx
+++ apps/client/components/searchCityInput/SearchItem.tsx
@@ -5,8 +5,10 @@ interface Props {
 
 export const SearchItem = ({ name, onAddCity }: Props) => {
   return (
-    <li className='p-4 hover:bg-gray-700 text-center cursor-pointer' onClick={() => onAddCity(name)}>
-      {name}
+    <li className='hover:bg-gray-700 text-center cursor-pointer'>
+      <button type='button' className='w-full h-full p-4' onClick={() => onAddCity(name)}>
+        {name}
+      </button>
     </li>
   )
 }
