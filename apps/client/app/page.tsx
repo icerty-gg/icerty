591947323dbf2c70555c0d35bd9903fc54065a61
Mateusz <michalikkontakt@gmail.com>
2022-12-28 06:43:19 +0100

    fix: fixed styles and edited interface structure

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,11 +1,11 @@
-import { BiLockAlt } from 'react-icons/bi'
+import { BiLockAlt, BiSearchAlt2 } from 'react-icons/bi'
 
 import { CategoryItem } from '../components/categories/CategoryItem'
 import { Offer } from '../components/offers/Offer'
+import { SearchCityInput } from '../components/searchCityInput/SearchCityInput'
 import { Container } from '../components/ui/Container'
 import { Heading } from '../components/ui/Heading'
 import { Layout } from '../components/ui/Layout'
-import { MainSearch } from '../components/ui/MainSearch'
 import { PrimaryButton } from '../components/ui/PrimaryButton'
 import { SecondaryButton } from '../components/ui/SecondaryButton'
 import { UserStats } from '../components/userStats/UserStats'
@@ -15,37 +15,56 @@ const Home = async () => {
   const { data: categories } = await api.get('/categories/')
   const { data: offers } = await api.get('/offers/')
 
-  const promotedOffers = offers.filter(f => f.isPromoted).slice(0, 10)
+  console.log(offers.forEach(o => console.log(o.images[0])))
 
   return (
     <Layout>
       <div className='grid grid-cols-2 gap-4'>
-        <Container className='col-span-2 z-20'>
+        <Container className='col-span-2 z-20 bg-transparent'>
           <Heading title='Find offers' className='pb-6' />
-          <MainSearch />
+          <form>
+            <div className='grid grid-cols-2 gap-4 max-md:grid-cols-1'>
+              <div className='flex relative items-center'>
+                <input
+                  type='text'
+                  placeholder='Search offer'
+                  className='border bg-gray-800/20 border-slate-800 hover:border-sky-400/20 rounded-full p-4 focus:outline-none focus:border-sky-400/20 text-white pl-12 w-full'
+                />
+                <BiSearchAlt2 className='absolute left-4 text-white text-xl' />
+              </div>
+              <div className='flex items-center gap-4 max-md:flex-col'>
+                <SearchCityInput />
+                <PrimaryButton isFormTypeButton={true} href='/offers'>
+                  Find
+                </PrimaryButton>
+              </div>
+            </div>
+          </form>
         </Container>
 
-        <Container className='max-lg:col-span-2'>
-          <Heading title='Categories' className='pb-6' />
-          <ul className='sticky grid grid-cols-3 max-lg:grid-cols-2 gap-4 max-md:grid-cols-1 backdrop-blur max-h-[35rem] overflow-hidden overflow-y-scroll'>
-            {categories.map(c => (
-              <CategoryItem href={c.name} key={c.id} name={c.name} image={c.img} />
-            ))}
-          </ul>
-        </Container>
+        <div className='grid grid-cols-[1fr,_2fr] col-span-2 gap-4 max-lg:grid-cols-1'>
+          <Container>
+            <Heading title='Categories' className='pb-6' />
+            <ul className='sticky grid grid-cols-2 max-lg:grid-cols-2 gap-4 max-md:grid-cols-1 backdrop-blur max-h-[35rem] overflow-hidden overflow-y-scroll'>
+              {categories.map(c => (
+                <CategoryItem href={c.name} key={c.id} image={c.img} {...c} />
+              ))}
+            </ul>
+          </Container>
 
-        <Container className='max-lg:col-span-2'>
-          <div className='flex items-center justify-center gap-4 pb-6'>
-            <Heading title='Promoted offers' />
-            <SecondaryButton href='/offers'>Check all</SecondaryButton>
-          </div>
+          <Container>
+            <div className='flex items-center justify-center gap-4 pb-6'>
+              <Heading title='Promoted offers' />
+              <SecondaryButton href='/offers'>Check all</SecondaryButton>
+            </div>
 
-          <ul className='sticky grid grid-cols-1 gap-4 backdrop-blur max-h-[35rem] overflow-hidden overflow-y-scroll min-w-[20rem]'>
-            {promotedOffers.map(o => {
-              return <Offer key={o.id} image={o.images[0].img} {...o} />
-            })}
-          </ul>
-        </Container>
+            <ul className='sticky grid grid-cols-1 gap-4 backdrop-blur max-h-[35rem] overflow-hidden overflow-y-scroll min-w-[20rem]'>
+              {offers.map(o => {
+                return <Offer image={o.images[0]?.img} key={o.id} {...o} />
+              })}
+            </ul>
+          </Container>
+        </div>
 
         <Container className='col-span-2'>
           <Heading title='Your offers' className='pb-6' />
