81830bd161ee2fb4fe082d0b1b67f837a2d7f312
Mateusz <michalikkontakt@gmail.com>
2023-01-11 12:53:41 +0100

    added categories from api

--- apps/client/components/offers/Offer.tsx
+++ apps/client/components/offers/Offer.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
-import { BiHeart, BiLocationPlus, BiCategoryAlt } from 'react-icons/bi'
+import { BiLocationPlus, BiCategoryAlt } from 'react-icons/bi'
 
-import { useRippleClickEffect } from '../../hooks/useRippleClickEffect'
+import { FollowButton } from '../ui/FollowButton'
 interface Props {
+  readonly category: { readonly id: string; readonly img: string; readonly name: string }
   readonly city: string
   readonly id: string
   readonly image: any
@@ -14,25 +16,35 @@ interface Props {
   readonly price: number
 }
 
-export const Offer = ({ city, id, image, isPromoted, name, price }: Props) => {
-  const { coords, createRippleHandler, isRipple } = useRippleClickEffect()
-
+export const Offer = ({ category, city, id, image, isPromoted, name, price }: Props) => {
   return (
-    <div className='relative overflow-hidden'>
-      <Link onClick={createRippleHandler} href={`/offers/${id}`}>
-        <li className='flex items-center gap-6 border transition-colors bg-gray-800/20 border-slate-800 hover:bg-sky-800/10 rounded-xl overflow-hidden'>
+    <div className='relative'>
+      <Link href={`/offers/${id}`}>
+        <li
+          className={clsx(
+            'flex items-center gap-6 border transition-colors bg-gray-800/20 border-slate-800 hover:bg-sky-800/10 rounded-xl',
+            isPromoted && 'border bg-sky-800/10 hover:bg-sky-800/20 border-sky-500/40'
+          )}
+        >
           <Image
             width={210}
             height={210}
             src={image}
             alt={name}
-            className='rounded-md object-cover max-md:max-w-[8rem] pointer-events-none'
+            className='rounded-md object-cover max-md:max-w-[8rem] pointer-events-none min-h-[14rem] max-h-[14rem]'
           />
 
           <div className='realtive p-4'>
             <div className='flex items-center justify-between w-full'>
               <div className='text-white flex flex-col gap-2'>
-                <h3 className='line-clamp-none md:line-clamp-2 text-xl'>{name}</h3>
+                <div className='flex items-center gap-4'>
+                  <h3 className='line-clamp-none md:line-clamp-2 text-xl'>{name}</h3>
+                  {isPromoted && (
+                    <p className='py-2 px-4 border bg-sky-500/60 border-sky-500/90 rounded-full text-white text-sm max-lg:text-xs text-center z-10'>
+                      Promoted offer
+                    </p>
+                  )}
+                </div>
                 <p className='text-2xl font-bold text-white'>
                   {price} <span className='text-sm'>USD</span>
                 </p>
@@ -40,39 +52,18 @@ export const Offer = ({ city, id, image, isPromoted, name, price }: Props) => {
             </div>
 
             <div className='absolute bottom-4 right-4 flex items-center gap-2'>
-              <p className=' flex items-center gap-2 text-sm text-sky-600 bg-sky-400/10 rounded-full py-2 px-4'>
-                <BiCategoryAlt className='text-lg' /> Furniture
+              <p className='flex items-center gap-2 text-sm text-sky-600 bg-sky-400/10 rounded-full py-2 px-4'>
+                <BiCategoryAlt className='text-lg' /> {category.name}
               </p>
 
-              <p className=' flex items-center gap-2 text-sm text-sky-600 bg-sky-400/10 rounded-full py-2 px-4'>
+              <p className='flex items-center gap-2 text-sm text-sky-600 bg-sky-400/10 rounded-full py-2 px-4'>
                 <BiLocationPlus className='text-lg' /> {city}
               </p>
             </div>
-
-            {isPromoted && (
-              <h3 className='absolute -top-2 -right-2 py-2 px-4 bg-sky-500 rounded-full text-white text-sm max-lg:text-xs text-center'>
-                Promoted offer
-              </h3>
-            )}
           </div>
         </li>
-
-        {isRipple && (
-          <div
-            style={{
-              left: coords.x,
-              top: coords.y
-            }}
-            className='absolute animate-[ripple_1.5s_ease_forwards] bg-sky-500/20 rounded-full translate-x-[-50%] translate-y-[-50%] pointer-events-none'
-          />
-        )}
       </Link>
-      <button
-        onClick={() => console.log(id)}
-        className='absolute top-4 right-4 flex items-center justify-center rounded-full p-2 text-sky-600 border bg-sky-400/10 border-slate-800 hover:bg-sky-400/20 hover:border-sky-500 transition-all'
-      >
-        <BiHeart className='text-lg' />
-      </button>
+      <FollowButton id={id} className='absolute top-4 right-4' isFollowed={true} />
     </div>
   )
 }
