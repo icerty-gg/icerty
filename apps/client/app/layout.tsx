3e32c53f8be41d66180d44cc9889c7ce64989a15
Mateusz <michalikkontakt@gmail.com>
2023-01-17 22:12:13 +0100

    feat: added react query to login and logout

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -3,11 +3,17 @@
 import '../styles/globals.css'
 import 'react-toastify/dist/ReactToastify.css'
 import { Roboto } from '@next/font/google'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { Suspense } from 'react'
 import { BiArrowBack } from 'react-icons/bi'
 import { ToastContainer } from 'react-toastify'
 
 import { Navbar } from '../components/navbar/Navbar'
 import { useCheckScroll } from '../hooks/useCheckScroll'
+
+import Loading from './loading'
+
+import type { ReactNode } from 'react'
 
 const roboto = Roboto({
   subsets: ['latin'],
@@ -15,27 +21,32 @@ const roboto = Roboto({
   variable: '--font-inter'
 })
 
-const RootLayout = ({ children }: { readonly children: React.ReactNode }) => {
+const RootLayout = ({ children }: { readonly children: ReactNode }) => {
   const isVisible = useCheckScroll(800)
+  const queryClient = new QueryClient()
 
   return (
     <html className={`${roboto.variable} font-sans`} lang='en'>
       <body className='bg-gray-900 relative'>
-        <Navbar />
-        <div className='max-w-screen-2xl w-full my-0 mx-auto px-8 max-md:px-4 py-24'>
-          {children}
-          <div className='absolute top-28 right-0 w-[50rem] h-[50rem] rounded-full blur-[250px] pointer-events-none bg-sky-500 opacity-10' />
-        </div>
-        <button
-          className={`fixed bottom-12 right-4 transition-transform ${
-            isVisible ? 'translate-x-[0]' : 'translate-x-[150%]'
-          } flex gap-2 items-center text-white bg-sky-500/10 hover:bg-sky-400/20 py-2 px-4 rounded-full border border-slate-300/10`}
-          onClick={() => (document.documentElement.scrollTop = 0)}
-        >
-          <BiArrowBack className='rotate-90' />
-          <p>Top</p>
-        </button>
-        <ToastContainer />
+        <QueryClientProvider client={queryClient} >
+          <Navbar />
+          <Suspense fallback={<Loading />}>
+            <main className='max-w-screen-2xl w-full my-0 mx-auto px-8 max-md:px-4 py-32'>
+              {children}
+              <div className='absolute top-28 right-0 w-[50rem] h-[50rem] rounded-full blur-[250px] pointer-events-none bg-sky-500 opacity-10' />
+            </main>
+          </Suspense>
+          <button
+            className={`fixed bottom-12 right-4 transition-transform ${
+              isVisible ? 'translate-x-[0]' : 'translate-x-[150%]'
+            } flex gap-2 items-center text-white bg-sky-500/10 hover:bg-sky-400/20 py-2 px-4 rounded-full border border-slate-300/10`}
+            onClick={() => (document.documentElement.scrollTop = 0)}
+          >
+            <BiArrowBack className='rotate-90' />
+            <p>Top</p>
+          </button>
+          <ToastContainer />
+        </QueryClientProvider>
       </body>
     </html>
   )
--- apps/client/components/navbar/Navbar.tsx
+++ apps/client/components/navbar/Navbar.tsx
@@ -1,15 +1,22 @@
 'use client'
 
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import clsx from 'clsx'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useRef } from 'react'
-import { BiHomeAlt, BiBriefcase, BiHeart, BiPlus, BiUser } from 'react-icons/bi'
+import { CiHeart, CiDollar, CiHome, CiPaperplane, CiUser, CiStar, CiLogout, CiLogin } from 'react-icons/ci'
 
 import { useCheckScroll } from '../../hooks/useCheckScroll'
+import { useUser } from '../../hooks/useUser'
 import Logotype from '../../public/logo.svg'
+import { api } from '../../utils/fetcher'
+import { notify } from '../../utils/notifications'
 import { PrimaryLink } from '../ui/primary-button/PrimaryLink'
 
 import { NavLink } from './NavLink'
+
+import type { ReactNode } from 'react'
 
 interface Props {
   readonly className?: string
@@ -23,16 +30,112 @@ export const Logo = ({ className, isHidden }: Props) => (
   </Link>
 )
 
+interface PopupLinkProps {
+  readonly href: string
+  readonly icon: ReactNode
+  readonly title: string
+}
+
+const PopupLinksData = [
+  {
+    title: 'Your account',
+    href: '/user',
+    icon: <CiUser className='text-2xl' />
+  },
+  {
+    title: 'Your offers',
+    href: '/user/offers',
+    icon: <CiDollar className='text-2xl' />
+  },
+  {
+    title: 'Your lists',
+    href: '/user/lists',
+    icon: <CiHeart className='text-2xl' />
+  },
+  {
+    title: 'Your opinions',
+    href: '/user/opinions',
+    icon: <CiStar className='text-2xl' />
+  }
+]
+
+const PopupLink = ({ href, icon, title }: PopupLinkProps) => {
+  return (
+    <li>
+      <Link className='text-white flex items-center gap-4 p-2 rounded-full hover:bg-gray-800/40' href={href}>
+        {icon} {title}
+      </Link>
+    </li>
+  )
+}
+
+interface NavbarLinkProps {
+  readonly href: string
+  readonly icon: ReactNode
+  readonly isSmallerNavbar: boolean
+  readonly title: string
+}
+
+const NavbarLinksData = [
+  {
+    title: 'Home',
+    href: '/',
+    icon: <CiHome className='text-2xl' />
+  },
+  {
+    title: 'Offers',
+    href: '/offers',
+    icon: <CiDollar className='text-2xl' />
+  },
+  {
+    title: 'Lists',
+    href: '/lists',
+    icon: <CiHeart className='text-2xl' />
+  },
+  {
+    title: 'Add offer',
+    href: '/add-offer',
+    icon: <CiPaperplane className='text-2xl' />
+  }
+]
+
+const NavbarLink = ({ href, icon, isSmallerNavbar, title }: NavbarLinkProps) => {
+  return (
+    <NavLink href={href}>
+      {icon}
+
+      <p className={clsx('text-xs transition-all', isSmallerNavbar && 'translate-y-[-0.5rem] invisible opacity-0')}>
+        {title}
+      </p>
+    </NavLink>
+  )
+}
+
 export const Navbar = () => {
+  const queryClient = useQueryClient()
+  const router = useRouter()
+  const { user } = useUser()
   const isSmallerNavbar = useCheckScroll(80)
   const navbarRef = useRef<HTMLElement | null>(null)
+
+  const { mutate } = useMutation({
+    mutationFn: () => api.post('/sessions/logout', undefined),
+    onSuccess: () => {
+      router.push('/')
+      queryClient.setQueryData(['user'], null)
+      notify('Successfully logout', 'success')
+    },
+    onError: () => {
+      notify('Error', 'error')
+    }
+  })
 
   return (
     <>
       <nav
         ref={navbarRef}
         className={clsx(
-          'fixed text-center bg-gray-900/90 backdrop-blur w-full h-20 flex items-center border-b border-slate-300/10 z-30 transition-all supports-backdrop-blur:bg-white/95',
+          'fixed text-center bg-gray-900/90 backdrop-blur w-full h-20 flex items-center border-b border-slate-300/10 z-30 transition-all supports-backdrop-blur:bg-white/95 duration-300',
           isSmallerNavbar && 'h-16'
         )}
       >
@@ -43,28 +146,48 @@ export const Navbar = () => {
         >
           <Logo isHidden={isSmallerNavbar} />
           <div className='flex items-center'>
-            <NavLink href='/'>
-              <BiHomeAlt />
-            </NavLink>
-            <NavLink href='/offers'>
-              <BiBriefcase />
-            </NavLink>
-            <NavLink href='/followed'>
-              <BiHeart />
-            </NavLink>
-            <NavLink href='/login'>
-              <BiUser />
-            </NavLink>
-            <NavLink href='/add-offer'>
-              <BiPlus />
-            </NavLink>
+            {NavbarLinksData.map(l => {
+              return <NavbarLink key={l.title} {...l} isSmallerNavbar={isSmallerNavbar} />
+            })}
           </div>
+          {user ? (
+            <div className='relative hover:bg-gray-900 border border-transparent hover:border-slate-300/10 border-b-0 rounded-lg rounded-b-none group ml-4'>
+              <NavLink href={`/user/${user.id}`}>
+                <CiUser className='text-2xl' />
 
-          <div className='flex gap-3 items-center text-sm max-lg:hidden'>
-            <PrimaryLink href='/register'>
-              <BiUser className='text-xl' /> Sign in
-            </PrimaryLink>
-          </div>
+                <p
+                  className={clsx(
+                    'text-xs transition-all',
+                    isSmallerNavbar && 'translate-y-[-0.5rem] invisible opacity-0'
+                  )}
+                >
+                  Account
+                </p>
+              </NavLink>
+
+              <div className='absolute p-4 right-0 top-[98%] w-[20rem] -z-10 bg-gray-900 border border-slate-300/10 rounded-lg rounded-tr-none invisible opacity-0 flex flex-col gap-4 group-hover:visible group-hover:opacity-100 transition-all'>
+                <div className='flex flex-col items-start gap-[0.1rem]'>
+                  <p className='text-gray-500'>Hello</p>
+                  <p className='text-xl text-white first-letter:uppercase'>{user.name}</p>
+                </div>
+
+                <ul className='grid grid-cols-1'>
+                  {PopupLinksData.map(l => {
+                    return <PopupLink key={l.title} {...l} />
+                  })}
+                </ul>
+                <button onClick={() => mutate()} className='flex items-center justify-center gap-2'>
+                  <CiLogout className='text-xl' /> Logout
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className='flex gap-3 items-center text-sm max-lg:hidden'>
+              <PrimaryLink href='/login'>
+                <CiLogin className='text-xl' /> Login
+              </PrimaryLink>
+            </div>
+          )}
         </div>
       </nav>
     </>
