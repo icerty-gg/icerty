185646250b9a98374713b27fde4aeac20b323b75
Stanislaw Jakubowski <sjakubowski3554@gmail.com>
2023-04-11 23:14:35 +0200

    Delete old files, add pnpm

---
+++ apps/client/components/Form/input/Input.stories.tsx
@@ -0,0 +1,25 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { BiLockAlt } from 'react-icons/bi'
+import { Input } from './Input'
+
+const meta: Meta<typeof Input> = {
+  title: 'Form/Input',
+  component: Input
+}
+
+export default meta
+
+type Story = StoryObj<typeof Input>
+
+export const Password: Story = {
+  args: {
+    icon: <BiLockAlt className='text-lg' />
+  }
+}
+
+export const Error: Story = {
+  args: {
+    icon: <BiLockAlt className='text-lg' />,
+    errorMessage: 'Password is invalid'
+  }
+}
