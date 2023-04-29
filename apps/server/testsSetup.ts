753b7420f3f1894ca34bb633f38ebed4e3b147e3
Stanislaw Jakubowski <sjakubowski3554@gmail.com>
2023-04-29 19:38:39 +0200

    Add code coverage in backend

--- apps/server/testsSetup.ts
+++ apps/server/testsSetup.ts
@@ -2,8 +2,17 @@ import { afterAll, afterEach, beforeAll } from "vitest";
 
 import fastify from "./app";
 
+const clearDatabase = async () => {
+	await fastify.prisma.offerImage.deleteMany();
+	await fastify.prisma.offer.deleteMany();
+	await fastify.prisma.category.deleteMany();
+	await fastify.prisma.user.deleteMany();
+	await fastify.prisma.followedOffers.deleteMany();
+};
+
 beforeAll(async () => {
 	await fastify.ready();
+	await clearDatabase();
 });
 
 afterAll(async () => {
@@ -11,9 +20,5 @@ afterAll(async () => {
 });
 
 afterEach(async () => {
-	await fastify.prisma.offerImage.deleteMany();
-	await fastify.prisma.offer.deleteMany();
-	await fastify.prisma.category.deleteMany();
-	await fastify.prisma.user.deleteMany();
-	await fastify.prisma.followedOffers.deleteMany();
+	await clearDatabase();
 });
